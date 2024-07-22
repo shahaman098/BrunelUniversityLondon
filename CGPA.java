7d2b17852356d267e72eee3e5543466a9e5d23f3
Aman Kumar Sah <118824856+shahaman098@users.noreply.github.com>
2024-07-22 20:31:09 +0100

    Add files via upload

---
+++ CGPA.java
@@ -0,0 +1,14 @@
+
+public class CGPA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		float subject1 = 10;
+		float subject2 = 30;
+		float subject3 = 30;
+		
+		float cgpa = (subject1+ subject2+ subject3);
+	}
+
+}
